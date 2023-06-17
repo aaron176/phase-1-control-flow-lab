073734fa3c98c059aeaa4058e14983f4bcac0f9d
haroon <haroon.seena2017@gmail.com>
2023-06-17 23:40:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  // Write your c let greode here!
+
+  if(distance === 0){
+    return "hello";
+  }
+  else if(distance > 0 && distance < 5){
+    return "hey";
+  }
+  else {
+    return "by"
+  }
+
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
   // Write your code here!
+  return destination === "hawai"? 'lets go' : 'never' ;
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(height){
   // Write your code here!
+  switch(height){
+    case 72:
+      return 'tall';
+      case 70:
+      return 'average';
+      case 68:
+        return 'below average';
+        default:
+          return 'short'
+
+  }
 }
+console.log(scuberGreetingForFeet(2))
